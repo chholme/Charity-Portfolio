cdc1f0061189100fa55cd530fdf65a10a3f774f5
chholme <chholme@iu.edu>
2024-05-22 19:55:25 -0400

    pls

---
+++ foolingAround.js
@@ -0,0 +1,5 @@
+const viewportWidth = window.innerWidth;
+const viewportHeight = window.innerHeight;
+
+console.log("Viewport Width:", viewportWidth);
+console.log("Viewport Height:", viewportHeight);
