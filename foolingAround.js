f9b0dae1becbf6210b1930c083647082483623af
chholme <chholme@iu.edu>
2024-05-22 21:46:54 -0400

    adding website updates

--- foolingAround.js
+++
@@ -1,5 +0,0 @@
-const viewportWidth = window.innerWidth;
-const viewportHeight = window.innerHeight;
-
-console.log("Viewport Width:", viewportWidth);
-console.log("Viewport Height:", viewportHeight);
